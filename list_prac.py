fcd46257146da584a3a4cc07083e52509d97d41f
ABRAHAM IKOKONYI <abrahamomusungu@gmail.com>
2025-11-27 11:22:32 +0300

    initial commit

---
+++ list_prac.py
@@ -0,0 +1,17 @@
+#list of numbers
+list_a = [1, 2, 3, "A banana", 5]
+list_b = [6, 7, "that is", 9, 10]
+list_c = ( 1 , "good", 2.3)
+list_d = ("apple", "goes for", "cherry")
+list_e = ( 1,3,(5,6) , 10)
+print (list_a[3])
+print (list_b[2])
+print (list_c[1])
+print (list_d[1])
+print ( list_e[3])
+del (list_a[3])
+print (list_a)
+list_a.append("Mango")
+print (list_a)  
+list_a.insert(33,312)
+print (list_a)
---
+++ set.py
@@ -0,0 +1,8 @@
+student_id = {345, 123, 243, 2345, 980, 234 }
+print(student_id)
+a = {1,2,3,4,5}
+b = {4,5,6,7,8}
+print("Union:", a | b)
+print("Intersection:", a & b)
+print("Difference:", a - b)
+print("Difference:", b - a)
